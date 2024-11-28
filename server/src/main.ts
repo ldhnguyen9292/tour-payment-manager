8a09a3b5a3650c06a98923509b6d0ea127952d52
Nguyen Le <ldhnguyen9292@gmail.com>
2024-11-28 17:19:22 +0700

    Add conditional Swagger setup based on environment and enable CORS for production

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,9 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as cookieParser from 'cookie-parser';
 import * as session from 'express-session';
 import * as passport from 'passport';
 
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -26,16 +26,23 @@ async function bootstrap() {
   app.use(passport.initialize());
   app.use(passport.session());
 
-  // Swagger configuration
-  const config = new DocumentBuilder()
-    .setTitle('Tour Payment Manager API')
-    .setDescription(
-      'The API documentation for the Tour Payment Manager application',
-    )
-    .setVersion('1.0')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api-docs', app, document); // Swagger UI will be hosted at /api-docs
+  if (process.env.NODE_ENV === 'production') {
+    app.enableCors({
+      origin: process.env.FRONTEND_URL,
+      credentials: true,
+    });
+  } else {
+    // Swagger configuration
+    const config = new DocumentBuilder()
+      .setTitle('Tour Payment Manager API')
+      .setDescription(
+        'The API documentation for the Tour Payment Manager application',
+      )
+      .setVersion('1.0')
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api-docs', app, document); // Swagger UI will be hosted at /api-docs
+  }
 
   await app.listen(process.env.PORT ?? 3000);
 }
